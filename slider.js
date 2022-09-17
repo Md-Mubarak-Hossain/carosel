6712ad1d810cb6c0cf135bccea78ab1554db2cc1
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-09-17 09:21:41 +0600

    corosel create

---
+++ slider.js
@@ -0,0 +1,19 @@
+const images = [
+    'images/cover-1.png',
+    'images/cover-2.jpg',
+    'images/cover-3.jpg'
+
+]
+let img = document.getElementById('carosel');
+img.setAttribute('height', 600);
+img.setAttribute('width', 1260);
+
+let image = 0;
+setInterval(() => {
+    if (image === images.length) {
+        image = 0;
+    }
+    img.setAttribute('src', (images[image]));
+    image++;
+
+}, 2000)
